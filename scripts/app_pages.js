e0feb9bf17d5952cbfd81ef9bdf5415087d429e3
sanhacheong <sanha.cheong@gmail.com>
2017-06-01 21:21:57 +0900

    window.event.clientY hover -> menu bar appears

--- scripts/app_pages.js
+++ scripts/app_pages.js
@@ -1,28 +1,27 @@
 // smooth scrolling
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function() {
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
 
-	// Make sure this.hash has a value before overriding default behavior
-	if (this.hash !== "") {
-	  // Prevent default anchor click behavior
-	  event.preventDefault();
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-	  // Store hash
-	  var hash = this.hash;
+			// Store hash
+			var hash = this.hash;
 
-	  // Using jQuery's animate() method to add smooth page scroll
-	  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	  $('html, body').animate({
-		scrollTop: $(hash).offset().top
-	  }, 1000, function(){
-   
-		// Add hash (#) to URL when done scrolling (default click behavior)
-		window.location.hash = hash;
-	  });
-	} // End if
-  });
-});
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate(
+				{scrollTop: $(hash).offset().top},
+				1000, function() {
+					// Add htmlash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;}
+			);
+		} // End if
+	});
+})
 	
 
 
@@ -35,19 +34,20 @@ $(window).scroll(function() {
 	}
 });
 
-
-$(document).ready(function() {
-	$(".top-region").hover(
-		function() {
+$(document).mousemove(function(event) {
+	if (window.event.clientY < 100) {
+		$("ul.navbar").css("top", "0px");
+	} else {
+		if ($(window).scrollTop() > $(window).height()*0.4) {
 			$("ul.navbar").css("top", "0px");
-		}, function() {
+		} else {
 			$("ul.navbar").css("top", "-100px");
 		}
-	);
-
-	$("ul.navbar").mouseenter(
-		function() {
-			$(this).css("top", "0px");
-		}
-	);
+	}
 });
+
+$("ul.navbar").mouseenter(
+	function() {
+		$(this).css("top", "0px");
+	}
+);
